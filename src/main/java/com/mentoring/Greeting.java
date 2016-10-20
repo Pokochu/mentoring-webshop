db8f0cf3f746e0e0871701f45339793cabfbd597
Gabor Istvan Pogonyi <Gabor_Istvan_Pogonyi@epam.com>
2016-10-20 11:58:53 +0200

    Creating Login ftl template with bootstrap

--- src/main/java/com/mentoring/Greeting.java
+++ src/main/java/com/mentoring/Greeting.java
@@ -1,8 +1,10 @@
 package com.mentoring;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Gabor_Istvan_Pogonyi on 9/8/2016.
@@ -10,9 +12,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class Greeting {
 
-    @RequestMapping("/greeting")
-    @ResponseBody
-    public String greeting() {
-        return "Szijja Zoli!!";
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(@ModelAttribute("model")ModelMap model) {
+
+        return "login";
     }
 }
