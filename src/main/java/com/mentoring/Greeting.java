e41cdf6050216158eba18e58864e525d12eea71b
Gabor Istvan Pogonyi <Gabor_Istvan_Pogonyi@epam.com>
2016-10-20 11:59:20 +0200

    Revert "Creating Login ftl template with bootstrap"
    This reverts commit db8f0cf3f746e0e0871701f45339793cabfbd597.

--- src/main/java/com/mentoring/Greeting.java
+++ src/main/java/com/mentoring/Greeting.java
@@ -1,10 +1,8 @@
 package com.mentoring;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by Gabor_Istvan_Pogonyi on 9/8/2016.
@@ -12,9 +10,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class Greeting {
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login(@ModelAttribute("model")ModelMap model) {
-
-        return "login";
+    @RequestMapping("/greeting")
+    @ResponseBody
+    public String greeting() {
+        return "Szijja Zoli!!";
     }
 }
