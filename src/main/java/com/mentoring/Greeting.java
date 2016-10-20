445d8d7d1039d23c40602a1e8bd3c88df1f432a0
Gabor Istvan Pogonyi <Gabor_Istvan_Pogonyi@epam.com>
2016-10-20 15:04:09 +0200

    create login page with bootstrap and freemarker

--- src/main/java/com/mentoring/Greeting.java
+++ src/main/java/com/mentoring/Greeting.java
@@ -2,7 +2,7 @@ package com.mentoring;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Created by Gabor_Istvan_Pogonyi on 9/8/2016.
@@ -10,9 +10,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class Greeting {
 
-    @RequestMapping("/greeting")
-    @ResponseBody
-    public String greeting() {
-        return "Szijja Zoli!!";
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String login() {
+        return "login";
     }
 }
