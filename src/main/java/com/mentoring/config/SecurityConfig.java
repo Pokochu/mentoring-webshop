3e7b9bfe8f485ace9d1f43974c8c273dbcf496df
Gabor Istvan Pogonyi <Gabor_Istvan_Pogonyi@epam.com>
2016-10-27 17:08:19 +0200

    Add extension for CSS, JS and font files

--- src/main/java/com/mentoring/config/SecurityConfig.java
+++ src/main/java/com/mentoring/config/SecurityConfig.java
@@ -2,12 +2,16 @@ package com.mentoring.config;
 
 import com.mentoring.model.Role;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static com.mentoring.model.Role.CUSTOMER;
 
 @Configuration
 @EnableWebSecurity
@@ -15,13 +19,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.
-                authorizeRequests()
-                    .antMatchers("/", "/home").permitAll()
-                    .anyRequest().authenticated()
-                    .and()
-                .formLogin()
-                    .failureUrl("/login?error")
+        http
+                .authorizeRequests()
+                .antMatchers("/css/**", "*/js/**", "/fonts/**").permitAll();
+        http
+                .formLogin().failureUrl("/login?error")
                     .loginPage("/login")
                     .permitAll()
                     .and()
@@ -31,6 +33,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("customer@ab.com").password("password").roles(Role.CUSTOMER.name());
+        auth.inMemoryAuthentication().withUser("customer@ab.com").password("password").roles(CUSTOMER.name().toLowerCase());
     }
 }
